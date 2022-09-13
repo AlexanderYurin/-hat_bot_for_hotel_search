d7a8218a69d6e4de035b5bda0071b781b864adc4
Alex Yurin <hiimmandarin32@gmail.com>
2022-09-13 11:36:32 +0300

    Added disable_web_page_preview

--- handlers/default_heandlers/history.py
+++ handlers/default_heandlers/history.py
@@ -6,4 +6,6 @@ from loader import bot
 
 @bot.message_handler(commands='history')
 def history(message: Message) -> None:
-    bot.send_message(message.from_user.id, f'История загружается...\n{database.history(message.from_user.id)}')
+    bot.send_message(message.from_user.id,
+                     f'История загружается...\n{database.history(message.from_user.id)}',
+                     disable_web_page_preview=True)
