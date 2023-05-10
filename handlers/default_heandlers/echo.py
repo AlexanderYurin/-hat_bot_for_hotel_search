f70053ef2e9c4e3ff93480ff65588645c747ac74
Alexander Yurin <hiimmandarin32@gmail.com>
2023-05-10 19:04:47 +0300

    Added doc

--- handlers/default_heandlers/echo.py
+++ handlers/default_heandlers/echo.py
@@ -1,9 +1,22 @@
 from telebot.types import Message
+from logger.logger import logger_bot
 
 from loader import bot
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
-def bot_echo(message: Message):
-    bot.reply_to(message, f'{message.from_user.first_name}, я тебя не понимаю.\nНажми /help ')
+@logger_bot
+def bot_echo(message: Message) -> None:
+    """
+    Эхо-хендлер для обработки текстовых сообщений без указанного состояния.
+
+    Принимает объект сообщения message типа Message из библиотеки Telebot.
+    Отвечает пользователю шаблонным сообщением, если не может понять запрос пользователя.
+
+    :param message: Объект сообщения типа Message из библиотеки Telebot.
+    :type message: telebot.types.Message
+    :return: Функция ничего не возвращает, сообщение отправляется с помощью метода bot.reply_to
+    :rtype: None
+    """
+    # Ответ на сообщение
+    bot.reply_to(message, f'{message.from_user.first_name}, я тебя не понимаю.\nНажми /help')
--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -2,9 +2,22 @@ from telebot.types import Message
 
 from config_data.config import DEFAULT_COMMANDS
 from loader import bot
+from logger.logger import logger_bot
 
 
 @bot.message_handler(commands=['help'])
-def bot_help(message: Message):
+@logger_bot
+def bot_help(message: Message) -> None:
+    """
+       Хендлер помощи для вывода всех доступных команд пользователю.
+
+       Принимает объект сообщения message типа Message из библиотеки Telebot.
+       Отвечает пользователю списком всех доступных команд.
+
+       :param message: Объект сообщения типа Message из библиотеки Telebot.
+       :type message: telebot.types.Message
+       :return: Функция ничего не возвращает, сообщение отправляется с помощью метода bot.reply_to
+       :rtype: None
+       """
     text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
     bot.reply_to(message, '\n'.join(text))
