10181df11f68c66598748972557e96e6500af54a
Alexander Yurin <hiimmandarin32@gmail.com>
2023-05-10 19:19:54 +0300

    Added doc

--- api_hotel/api.py
+++ api_hotel/api.py
@@ -1,26 +1,33 @@
 from typing import Dict
-
 import requests
-
 from config_data.config import RAPID_API_KEY
+from loguru import logger
+
+logger.add("connect.log", rotation="10 MB", level="INFO", format="{time} - {name} - {level} - {message}")
 
 
 def request_to_api(url: str, querystring: Dict) -> str:
     """
-    Эта функция возвращает результат полученный от сайта в формате строки.
-    :param querystring: отвечает за параметры запроса на сайт.
-    :param url: отвечает за ссылку на сайт.
+    Функция отправляет GET-запрос на заданный URL с заданными параметрами и заголовками.
+    :param url: Ссылка на сайт (строка).
+    :param querystring: Параметры запроса (словарь).
+    :return: Строка с ответом от сервера.
     """
-
     headers = {
         "X-RapidAPI-Key": RAPID_API_KEY,
         "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
-
     }
-    try:
-        response = requests.get(url, headers=headers, params=querystring, timeout=10)
-        if response.status_code == requests.codes.ok:
-            return response.text
 
-    except Exception:
-        print('Ошибка подключения к серверу!')
+    try:
+        with requests.get(url, headers=headers, params=querystring, timeout=30) as response:
+            response.raise_for_status()
+            logger.info(f'{url}: {response.status_code}!')
+            return response.text
+    except requests.exceptions.HTTPError as errh:
+        logger.exception(f'HTTP-ошибка: {errh}')
+    except requests.exceptions.ConnectionError as errc:
+        logger.exception(f'Ошибка подключения: {errc}')
+    except requests.exceptions.Timeout as errt:
+        logger.exception(f'Ошибка тайм-аута: {errt}')
+    except requests.exceptions.RequestException as err:
+        logger.exception(f'Что-то пошло не так: {err}')
--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -1,9 +1,23 @@
 from telebot.types import Message
+from logger.logger import logger_bot
 
 from loader import bot
 
 
 @bot.message_handler(commands=['start'])
-def bot_start(message: Message):
+@logger_bot
+def bot_start(message: Message) -> None:
+    """
+    Стартовый хендлер с привествием и информацией о боте.
+
+    Принимает объект сообщения message типа Message из библиотеки Telebot.
+    Отвечает пользователю шаблонным сообщением, если не может понять запрос пользователя.
+
+    :param message: Объект сообщения типа Message из библиотеки Telebot.
+    :type message: telebot.types.Message
+    :return: Функция ничего не возвращает, сообщение отправляется с помощью метода bot.reply_to
+    :rtype: None
+    """
     bot.reply_to(message, f'{message.from_user.first_name}, добро пожаловать!\n'
-                          f'Я помощник по подбору лучшего предложения в среде отелей для Вас!\n')
+                          f'Я помощник по подбору лучшего предложения в среде отелей для Вас!\n'
+                          f'Введи /help я расскажу что умею!')
